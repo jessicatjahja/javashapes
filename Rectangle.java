d062f1eb3b463bb61148db1e56813a1ee5f48486
Jessica Tjahja <agnes.j.tjahja@gmail.com>
2018-11-16 01:20:32 -0600

    added Rectangle base class

---
+++ Rectangle.java
@@ -0,0 +1,32 @@
+public class Rectangle extends Shape implements Printable{
+
+    protected int width;
+    protected int height;
+    protected int side1Length;
+    protected int side2Length;
+    protected double area = getArea(this.baseLength, this.height);
+    
+    public Rectangle(String name, int numSides, int baseLength, int side1Length, int side2Length, int height){
+        super(name, numSides);
+        this.baseLength = baseLength;
+        this.side1Length = side1Length;
+        this.side2Length = side2Length;
+        this.height = height;
+    }
+
+    public double getArea(int baseLength, int height){
+        double area = (baseLength * height);
+        return area;
+    }
+
+    @Override
+	public String toString() {
+        return String.format("Shape: %s%n Base: %d%n Side 1: %d%n Side 2: %d%n Height: %d%n Area: %d%n",
+        name, baseLength, side1Length, side2Length, height, area);
+	}
+
+	@Override
+	public void print() {
+		System.out.println(this.toString());		
+	}
+}
