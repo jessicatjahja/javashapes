fafe89c42d386b7ef25a91938d500e73a9f4e3b0
Jessica Tjahja <agnes.j.tjahja@gmail.com>
2018-11-16 01:53:53 -0600

    added Rhombus class

---
+++ Rhombus.java
@@ -0,0 +1,21 @@
+public class Rhombus extends Rectangle implements Printable{
+    
+    private int side;
+    private int height;    
+
+    public Rhombus(String name, int numSides, int side, int height){
+        super(name, numSides, side, side);
+        this.height = height;        
+    }
+
+    @Override
+	public String toString() {
+        return String.format("Shape: %s%n Sides: %d%n Height: %d%n Area: %d%n",
+        name, sides, height, super.getArea(side, height));
+	}
+
+	@Override
+	public void print() {
+		System.out.println(this.toString());
+	}
+}
